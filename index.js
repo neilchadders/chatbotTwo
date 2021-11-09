9a1be0bbfb38bdc13b1a82d5ebdd141e77877366
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2021-11-09 13:57:21 +0000

    removing unnecessary code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
-//openForm will open the chat box and ask inititial Questions?
+// openForm will open the chat box and ask inititial Questions
 
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
-  
-  const messagesContainer = document.getElementById("myForm");
+  //document.getElementById("myForm").style.display = "block";
+ 
+  const messagesContainer = document.getElementById("messages");
+  messagesContainer.style.display = "block";
   let initialDiv = document.createElement("div");
   initialDiv.id = "initialUser";
   initialDiv.className = "first questions";
-  initialDiv.innerHTML = `<span>Hi</span> <button>press1</button>` ;
+  initialDiv.innerHTML =  `Hi <break/><button>yes</button><break/><button>No</button> ` ;
   messagesContainer.appendChild(initialDiv);
 }
