7782651fe0a4cb325eabb04b0d32a8934ae621ef
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2021-11-09 06:17:24 +0000

    adding open button and initial chat bot questions

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+
+//openForm will open the chat box and ask inititial Questions?
+
 function openForm() {
   document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  
+  const messagesContainer = document.getElementById("myForm");
+  let initialDiv = document.createElement("div");
+  initialDiv.id = "initialUser";
+  initialDiv.className = "first questions";
+  initialDiv.innerHTML = `<span>Hi</span> <button>press1</button>` ;
+  messagesContainer.appendChild(initialDiv);
 }
